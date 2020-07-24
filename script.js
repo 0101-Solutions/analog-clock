63119b83402f0529ed8cfaa5533a6822987393fd
Toxic254 <cyrus_g@outlook.com>
2020-07-24 13:17:33 +0300

    Worked on adding the clock functionality with JS.

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 265;
+function runTheClock() {
+    var date = new Date();
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    // Working on dividing the clock into equal segments based on their properties.
+    let hrPosition = (hr * 360 / 12) + (min *(360 /60) / 12);
+    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    let secPosition = sec * 360 / 60;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+};
+
+var interval = setInterval(runTheClock, 1000);
